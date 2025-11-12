8c6984df3d04a3dafb2925be7afcb8039e99753e
Eric Yang <65512267+ericyang8@users.noreply.github.com>
2025-11-12 15:54:10 -0500

    Fixed map to singapore

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+// Create map and center on Singapore
+var map = L.map('map').setView([1.3521, 103.8198], 11);
 
+// Add the OpenStreetMap tile layer
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+// Add a simple marker (from your original code)
+var marker1 = L.marker([1.3521, 103.8198]).addTo(map);
+marker1.bindPopup("<b>Hello Singapore!</b><br>I am a popup.");
+
+
+// Include the GeoJson file -> include districts on the map
+fetch('region_map.geojson')
+    .then(function(response) {
+        return response.json(); // Convert the response to JSON
+    })
+    .then(function(data) {
+        // 'data' now contains your GeoJSON object
+        // Add the GeoJSON layer to the map
+        L.geoJson(data).addTo(map);
+    })
+    .catch(function(error) {
+        // Log any errors to the console
+        console.error('Error loading the GeoJSON file:', error);
+    });
